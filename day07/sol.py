9fd3cc1d09ffd58b14a36958e414898e357ef4b6
Yoav Caspi <yoavcaspi@gmail.com>
2021-12-08 06:57:08 +0200

    initial solution for day 7 part 1 and 2.

---
+++ day07/sol.py
@@ -0,0 +1,44 @@
+import argparse
+from collections import defaultdict
+from copy import deepcopy
+from typing import List, Tuple
+
+
+def sol(data: str) -> int:
+    count = 0
+    crabs = [int(val) for val in data.split(",")]
+    sums = []
+    for i in range(1000):
+        new_sum = 0
+        for crab in crabs:
+            new_sum += abs(i - crab)
+        sums.append(new_sum)
+    return min(sums)
+
+
+def get_input(filename: str) -> str:
+    with open(filename) as f:
+        lines = f.read()
+    return lines
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filename', default='input.txt')
+    args = parser.parse_args()
+    data = get_input(args.filename)
+    example()
+    print(sol(data))
+    return 0
+
+
+INPUT = """16,1,2,0,4,2,7,1,2,14
+"""
+
+
+def example():
+    assert sol(INPUT) == 37
+
+
+if __name__ == '__main__':
+    exit(main())
