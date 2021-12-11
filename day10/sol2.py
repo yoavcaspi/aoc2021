3afcae83b9f619db84729a9313ca8dc2e3bea95e
Yoav Caspi <yoavcaspi@gmail.com>
2021-12-11 06:58:18 +0200

    initial solution for day 10 part 1 and 2.

---
+++ day10/sol2.py
@@ -0,0 +1,81 @@
+import argparse
+from collections import defaultdict
+from copy import deepcopy
+from typing import List, Tuple
+
+
+def sol(data: str) -> int:
+    # 1 4 7 8
+    # 2 4 3 7
+    count = []
+
+    op = {"(", "[", "<", "{"}
+    cl = {")", "]", ">", "}"}
+    brackets = {
+        ")": "(",
+        "]": "[",
+        ">": "<",
+        "}": "{"
+    }
+    score = {"(": 1,
+             "[": 2,
+             "{": 3,
+             "<": 4}
+    for y, line in enumerate(data.splitlines()):
+        stack = []
+        for c in line:
+            if c in op:
+                stack.append(c)
+            else:
+                if brackets[c] == stack[-1]:
+                    stack.pop(-1)
+                else:
+                    break
+        else:
+            s = 0
+            for val in reversed(stack):
+                s*=5
+                s+=score[val]
+            print(stack, s)
+
+            count.append(s)
+    count.sort()
+    return count[len(count)//2]
+
+
+def get_input(filename: str) -> str:
+    with open(filename) as f:
+        lines = f.read()
+    return lines
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filename', default='input.txt')
+    args = parser.parse_args()
+    data = get_input(args.filename)
+    example()
+    print(sol(data))
+    return 0
+
+
+INPUT = """\
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+"""
+
+
+def example():
+    assert sol(INPUT) == 288957
+
+
+if __name__ == '__main__':
+    exit(main())
